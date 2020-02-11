1a641e50a5348071b74c02a9c6813e375f58a3ab
jossanjim3 <jossanjim3@gmail.com>
2020-02-11 19:08:59 +0100

    enu de application creado

--- api/models/applicationModel.js
+++ api/models/applicationModel.js
@@ -1,21 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-const Status =  {
-    PENDING : 'PENDING',
-    REJECTED : 'REJECTED',
-    DUE : 'DUE',
-    ACCEPTED : 'ACCEPTED',
-    CANCELLED : 'CANCELLED'
-}
-
 var applicationSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now
     },
     status:  {
-        type: Status,
+        type: String,
+        enum: ['PENDING','REJECTED','DUE','ACCEPTED','CANCELLED'],
         required: 'Kindly enter the status of the application'
     },
     comment:  {
