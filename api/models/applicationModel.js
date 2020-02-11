c4593a6ffbfecb872357ca5e97fe230bb6d5c9dd
jossanjim3 <jossanjim3@gmail.com>
2020-02-11 19:45:54 +0100

    applicationModel modificado segun las validaciones vistas en la clase 4

--- api/models/applicationModel.js
+++ api/models/applicationModel.js
@@ -1,7 +1,8 @@
+'use strict';
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var applicationSchema = new Schema({
+var ApplicationSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now
@@ -9,12 +10,26 @@ var applicationSchema = new Schema({
     status:  {
         type: String,
         enum: ['PENDING','REJECTED','DUE','ACCEPTED','CANCELLED'],
-        required: 'Kindly enter the status of the application'
+        required: 'Kindly enter the status of the application',
+        default : 'PENDING' // initial status always PENDING
     },
     comment:  {
         type: String,
     },
+    explorer: {
+        type: Schema.Types.ObjectId,
+        required: 'explorer id required'
+    },
+    trip: {
+        type: Schema.Types.ObjectId,
+        required: 'trip id required'
+    },
 
 }, {strict:false});
+
+// Execute before each item.save() call
+ApplicationSchema.pre('save', function(callback) {
+    // do nothing at the moment
+});
 
 module.exports = mongoose.model('Applications', applicationSchema)
