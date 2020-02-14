8b61195dc14b87f8052fd37f99cef8c6f9c9ac52
jossanjim3 <jossanjim3@gmail.com>
2020-02-14 12:30:16 +0100

    añadir las routes de application que faltaban

--- api/routes/applicationRoutes.js
+++ api/routes/applicationRoutes.js
@@ -3,13 +3,33 @@
 module.exports = function (app){
     var applications = require('../controllers/applicationController');
 
-    app.route('/applications')
-        .get(applications.list_all_applications)
+     app.route('v1/applications')
+        // list all the applications
+        .get(applications.list_all_applications)        
+        // create an applicaction
         .post(applications.create_an_application);
     
-    app.route('/applications/:applicationId')
+    app.route('v1/applications/:userId')
+        // list applications that explorers made
+        .get(applications.list_my_applications)
+
+    app.route('v1/applications/:applicationId')
+        // read an application
         .get(applications.read_an_application)
+        // update an application status
         .put(applications.update_an_application)
+        // delete an application. Currently an application cannot be deleted!
         .delete(applications.delete_an_application);
+    
+    app.route('v1/applications/:managerId')
+        // list application that manager manages
+        .get(applications.list_all_manager_applications)
+
+    app.route('v1/applications/:managerId/:applicationId')
+        // read an applicaction that manager manages
+        .get(applications.read_an_application_by_manager)
+        // update an applicaction status that manager manages
+        .put(applications.update_an_application_by_manager)
+
 
 }
