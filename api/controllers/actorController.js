cdacfd07af6d94265f9f9585c9fb76e28e4bd53c
kapy95 <41288942+kapy95@users.noreply.github.com>
2020-03-10 10:07:44 +0100

    Added V1 and V2 of actors in the routes and in the controller

--- api/controllers/actorController.js
+++ api/controllers/actorController.js
@@ -11,34 +11,9 @@ var authController = require('./authController');
 exports.create_an_actor = function(req,res){
     var new_actor = new Actor(req.body);
 
-    //checks that the user can register as explorer, manager (only an admin can create it) or sponsor
+    //checks that the user can register as explorer, manager or sponsor, it is not allowed to create actors as administrators
     if(new_actor.role.includes('ADMINISTRATOR')){
         res.status(422).send("No se puede crear un administrador")
-    }else if (new_actor.role.includes('MANAGER')){
-
-        var role=authController.getUserId(req.body.id);
-
-        if(role=='ADMINISTRATOR'){
-    
-                new_actor.save(function(err, actor) {
-
-                                        if (err){
-
-                                                res.status(500).send(err);
-                                        }
-                                        else{
-                                                res.json(actor);
-                                                res.status(200);
-                                        }
-                 });
-                                        
-                                        
-            }else{
-
-                res.status(403).send("El usuario no está autorizado");
-
-             }
-
     }else{
         new_actor.save(function(err, actor) {
             if (err){
@@ -77,15 +52,186 @@ exports.delete_an_actor = function(req, res) {
             res.status(500).send(err);
         }
         else{
-            res.json({ message: 'This actor  was successfully deleted' });
+            res.json({ message: 'The actor was successfully deleted' });
             res.status(204)
-            //res.json(actor); the actor deleted is sent just in case that he/she didn't want to delete that actor and he/she can see what it is being deleted.
+            
         }
     });
 };
 
 
 exports.update_an_actor = function(req,res){
+
+    var actor_body=req.body;
+    var promise_hash = new Promise((resolve,reject)=>{
+        if(actor_body.password!=undefined){
+            bcrypt.genSalt(5, function(err, salt) {
+                if (err) reject(err);
+            
+                bcrypt.hash(actor_body.password, salt, function(err, hash) {
+                  if (err) reject(err);
+                  actor_body.password = hash;
+                  resolve(actor_body)
+                });
+              });
+         }else{
+             resolve(actor_body)
+         }
+    })
+    
+    promise_hash.then((actor_body)=>{
+
+        Actor.findOneAndUpdate({_id: req.params.actorId}, actor_body, {new: true}, function(err, actor) {
+            if (err){
+              if(err.name=='ValidationError') {
+                  res.status(422).send(err);
+              }
+              else{
+    
+                 res.status(500).send(err);
+              }
+              
+            }
+            else{
+                res.json(actor);
+            }
+          });
+    }).catch(function(err){
+        res.status(err).send(err);
+    })
+
+};
+
+exports.modify_actor_validation = function(req, res) {
+    
+    if (req.body.validated.includes("true")){
+        console.log("Validating an actor with id: "+req.params.actorId)
+    }else{
+        console.log("Invalidating an actor with id: "+req.params.actorId)
+    }
+
+    Actor.findOneAndUpdate({_id: req.params.actorId},  { $set: {"validated":req.body.validated }}, {new: true}, function(err, actor) {
+        if (err){
+        res.status(500).send(err);
+        }
+        else{
+        res.json(actor);
+        res.status(200);
+        }
+    });
+    
+};
+
+
+exports.updateFinder = function(req, res) {
+    if(!req.params.role.contains('EXPLORER')){
+        res.status(422).json({message: 'The actor must be an explorer.'})
+    }
+    else{
+        var newFinder = req.body;
+        newFinder.timestamp = new Date();
+    }
+}
+
+
+
+//Authenticated version V2:
+
+exports.login_an_actor = async function(req, res) {
+    console.log('starting login an actor');
+    var emailParam = req.query.email;
+    var password = req.query.password;
+    Actor.findOne({ email: emailParam }, function (err, actor) {
+        if (err) { res.send(err); }
+  
+        // No actor found with that email as username
+        else if (!actor) {
+          res.status(401); //an access token isn’t provided, or is invalid
+          res.json({message: 'forbidden',error: err});
+        }
+        
+        //if the actor is not validated, he/she can not do any operation
+        else if (actor.validated == false) {
+          res.status(403); //an access token is valid, but requires more privileges: to be validated
+          res.json({message: 'forbidden',error: err});
+        }else{
+          // Make sure the password is correct
+          //console.log('En actor Controller pass: '+password);
+          actor.verifyPassword(password, async function(err, isMatch) {
+            if (err) {
+              res.send(err);
+            }
+  
+            // Password did not match
+            else if (!isMatch) {
+              //res.send(err);
+              res.status(401); //an access token isn’t provided, or is invalid
+              res.json({message: 'forbidden',error: err});
+            }
+            else {
+                try{
+                  var customToken = await admin.auth().createCustomToken(actor.email);
+                } catch (error){
+                  console.log("Error creating custom token:", error);
+                }
+                actor.customToken = customToken;
+                console.log('Login Success... sending JSON with custom token');
+                res.json(actor);
+            }
+        });
+      }
+    });
+  };
+
+exports.create_an_actor_authenticated = function(req,res){
+    var new_actor = new Actor(req.body);
+
+    //checks that the user can register as explorer, manager (only an admin can create it) or sponsor
+    if(new_actor.role.includes('ADMINISTRATOR')){
+        res.status(422).send("No se puede crear un administrador")
+    }else if (new_actor.role.includes('MANAGER')){
+
+        id=authController.getUserId(req.body.id);
+        
+        Actor.findById(id, function(err,actor){
+
+            if(actor.role.includes('ADMINISTRATOR')){
+        
+                    new_actor.save(function(err, actor) {
+
+                                            if (err){
+
+                                                    res.status(500).send(err);
+                                            }
+                                            else{
+                                                    res.json(actor);
+                                                    res.status(200);
+                                            }
+                    });
+                                            
+                                            
+                }else{
+
+                    res.status(403).send("El usuario no está autorizado");
+
+                }
+                });
+
+    }else{
+        new_actor.save(function(err, actor) {
+            if (err){
+
+                res.status(500).send(err);
+            }
+            else{
+                res.json(actor);
+                res.status(200);
+            }
+        });
+    }
+};
+
+exports.update_an_actor_authenticated = function(req,res){
     //Check that the user is the proper actor and if not: res.status(403); "an access token is valid, but requires more privileges"
     Actor.findById(req.params.actorId, async function(err, actor) {
         if (err){
@@ -146,82 +292,3 @@ exports.update_an_actor = function(req,res){
     });
     
 };
-
-exports.modify_actor_validation = function(req, res) {
-    
-    if (req.body.validated.includes("true")){
-        console.log("Validating an actor with id: "+req.params.actorId)
-    }else{
-        console.log("Invalidating an actor with id: "+req.params.actorId)
-    }
-    
-    //Check that the user is an Administrator and if not: res.status(403); "an access token is valid, but requires more privileges"
-
-    Actor.findOneAndUpdate({_id: req.params.actorId},  { $set: {"validated":req.body.validated }}, {new: true}, function(err, actor) {
-        if (err){
-        res.status(500).send(err);
-        }
-        else{
-        res.json(actor);
-        res.status(200);
-        }
-    });
-    
-};
-
-exports.login_an_actor = async function(req, res) {
-    console.log('starting login an actor');
-    var emailParam = req.query.email;
-    var password = req.query.password;
-    Actor.findOne({ email: emailParam }, function (err, actor) {
-        if (err) { res.send(err); }
-  
-        // No actor found with that email as username
-        else if (!actor) {
-          res.status(401); //an access token isn’t provided, or is invalid
-          res.json({message: 'forbidden',error: err});
-        }
-        
-        //if the actor is not validated, he/she can not do any operation
-        else if (actor.validated == false) {
-          res.status(403); //an access token is valid, but requires more privileges: to be validated
-          res.json({message: 'forbidden',error: err});
-        }else{
-          // Make sure the password is correct
-          //console.log('En actor Controller pass: '+password);
-          actor.verifyPassword(password, async function(err, isMatch) {
-            if (err) {
-              res.send(err);
-            }
-  
-            // Password did not match
-            else if (!isMatch) {
-              //res.send(err);
-              res.status(401); //an access token isn’t provided, or is invalid
-              res.json({message: 'forbidden',error: err});
-            }
-            else {
-                try{
-                  var customToken = await admin.auth().createCustomToken(actor.email);
-                } catch (error){
-                  console.log("Error creating custom token:", error);
-                }
-                actor.customToken = customToken;
-                console.log('Login Success... sending JSON with custom token');
-                res.json(actor);
-            }
-        });
-      }
-    });
-  };
-
-
-exports.updateFinder = function(req, res) {
-    if(!req.params.role.contains('EXPLORER')){
-        res.status(422).json({message: 'The actor must be an explorer.'})
-    }
-    else{
-        var newFinder = req.body;
-        newFinder.timestamp = new Date();
-    }
-}
--- api/routes/actorRoutes.js
+++ api/routes/actorRoutes.js
@@ -4,20 +4,44 @@ module.exports = function(app) {
   var authController=require('../controllers/authController');
 
   app.route('/v1/actors')
+  
     //create an actor as an explorer or  as a manager (it must do it an admin)
     .post(actors.create_an_actor);
     
   app.route('/v1/actors/:actorId')
+
     //Retrieve an actor 
     .get(actors.read_an_actor)
+
     //edit the personal data of the actor
-    .put(authController.verifyUser(["EXPLORER","MANAGER", "ADMINISTRATOR",
-    "SPONSORSHIP"]),actors.update_an_actor)
+    .put(actors.update_an_actor)
+
+    //delete an actor
     .delete(actors.delete_an_actor);
 
   app.route('/v1/actors/:actorId/validated')
-    //ban or unban an actor 
-    .put(authController.verifyUser(["ADMINISTRATOR"]),actors.modify_actor_validation);
 
+    //ban or unban an actor 
+    .put(actors.modify_actor_validation);
+
+
+  //V2: authentication
+
+  app.route('/v2/actors')
+
+    //create an actor as an explorer or  as a manager (it must do it an admin) with authentication
+    .post(actors.create_an_actor_authenticated);
+
+  app.route('/v2/actors/:actorId')
+
+    //edit the personal data of the actor with authentication
+    .put(authController.verifyUser(["EXPLORER","MANAGER", "ADMINISTRATOR",
+    "SPONSORSHIP"]),actors.update_an_actor_authenticated)
+
+  app.route('/v2/actors/:actorId/validated')
+
+    //validate or invalidate an actor with authentication
+    .put(authController.verifyUser(["ADMINISTRATOR"]),actors.modify_actor_validation);
+  
 
 };
